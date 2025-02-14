c207a82819b4e1ea41248f3740f4275a536cde73
felipealvx <felipealvk@gmail.com>
2025-02-13 23:38:06 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json())
+
+const users = []
+
+app.post("/usuarios", (req, res) => {
+  users.push(req.body)
+
+  res.status(201).json(req.body)
+})
+
+app.get("/usuarios", (req, res) => {
+  res.status(200).json(users)
+})
+
+app.listen(3000, () => {
+  console.log("Server executando na porta 3000!")
+})
